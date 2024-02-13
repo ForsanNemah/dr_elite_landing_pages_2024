52f46356c7c3912a3fcd77250b9fb6d34cac5700
ForsanNemah <forsan20172017@gmail.com>
2024-02-13 07:20:43 +0300

    aa

---
+++ qaser/lazer/head.php
@@ -0,0 +1,114 @@
+<head>
+  <!-- Meta Tags -->
+  <meta charset="utf-8">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="">
+  <!-- Page Title -->
+  <title>
+
+<?php
+
+include "info.php";
+
+echo $site_name;
+
+?>
+
+
+
+  </title>
+  <!-- Favicon Icon -->
+  <link rel="icon" href="assets/img/favicon.png" />
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+  <link rel="stylesheet" href="assets/css/fontawesome.css" />
+  <link rel="stylesheet" href="assets/css/slick.css" />
+  <link rel="stylesheet" href="assets/css/lightgallery.min.css" />
+  <link rel="stylesheet" href="assets/css/animate.css" />
+  <link rel="stylesheet" href="assets/css/jQueryUi.min.css" />
+  <link rel="stylesheet" href="assets/css/textRotate.css" />
+  <link rel="stylesheet" href="assets/css/select2.min.css" />
+  <link rel="stylesheet" href="assets/css/style.css" />
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+
+
+
+
+
+
+
+  <link href='https://fonts.googleapis.com/css?family=Tajawal' rel='stylesheet'>
+  <link href='style.css' rel='stylesheet'>
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+<?php
+
+include "info.php";
+include "slider.php";
+include "cta/cta_whatsapp.php";
+
+?>
+
+
+
+
+
+
+
+
+
+
+// tiktok
+<script>
+		!function (w, d, t) {
+		  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++
+)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var i="https://analytics.tiktok.com/i18n/pixel/events.js";ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=i,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=i+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+		
+		  ttq.load('CN4G4Q3C77U0CFANUN20');
+		  ttq.page();
+		}(window, document, 'ttq');
+	</script>
+
+
+
+//snap 
+<script>
+var event_phn="<?=$phn?>";
+var event_email="info@wmc-ksa.com";
+
+(function(e,t,n){if(e.snaptr)return;var a=e.snaptr=function()
+{a.handleRequest?a.handleRequest.apply(a,arguments):a.queue.push(arguments)};
+a.queue=[];var s='script';r=t.createElement(s);r.async=!0;
+r.src=n;var u=t.getElementsByTagName(s)[0];
+u.parentNode.insertBefore(r,u);})(window,document,
+'https://sc-static.net/scevent.min.js');
+
+snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
+
+snaptr('track', 'PAGE_VIEW');
+ 
+ 
+
+    </script>
+
+
+
+
+
+
+
+
+</head>
