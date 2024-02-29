440a315ad23248c6ff6cb198ca27bc65c71e856c
ForsanNemah <forsan20172017@gmail.com>
2024-02-29 10:24:08 +0300

    aa

---
+++ dkr_qr/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ 
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>منيو مطعم دلع كرشك </title>
+
+  <link href="https://fonts.cdnfonts.com/css/tajawal" rel="stylesheet">
+                
+</head>
+
+<body>
+  <div class="container">
+    <div class="text-center mt-5">
+      <img src="logo.jpeg" alt="Logo" class="img-fluid " width="150" height="150">
+      <h1 class="mt-4 pulsate">منيو مطعم دلع كرشك </h1>
+    </div>
+    <div class="text-center mt-5">
+      <a href="https://drive.google.com/file/d/1PhQlWHRyJRSuBw7lFUAyXNHhhWhxKAur/view" class="btn btn-primary btn-lg  col-6 ">منيو رمضان</a>
+<br>
+<br>
+<a href="https://drive.google.com/file/d/1PhQlWHRyJRSuBw7lFUAyXNHhhWhxKAur/view" class="btn btn-primary btn-lg col-6">المنيو العام </a>
+
+<br>
+<br>
+<a href="https://drive.google.com/file/d/1PhQlWHRyJRSuBw7lFUAyXNHhhWhxKAur/view" class="btn btn-primary btn-lg col-6"> منيو وجبات الشركات </a>
+     
+    </div>
+
+
+
+  </div>
+
+  <footer class="bg-light text-center mt-5">
+    <div class="container">
+      <ul class="list-inline">
+
+      
+
+
+        <li class="list-inline-item " style="font-size:30px">
+          <a href="https://www.facebook.com/dallakershaik/"><i class="fa fa-facebook"></i></a>
+        </li>
+        <li class="list-inline-item" style="font-size:30px">
+          <a href="https://twitter.com/dl3_kershk?lang=ar"><i class="fa fa-twitter"></i></a>
+        </li>
+        <li class="list-inline-item" style="font-size:30px">
+          <a href="https://www.instagram.com/dalaakershaik"><i class="fa fa-instagram"></i></a>
+        </li>
+        <li class="list-inline-item" style="font-size:30px">
+          <a href="https://www.snapchat.com/add/dkr_ksa"><i class="fa fa-snapchat"></i></a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+ 
+</body>
+
+
+<style>
+
+
+a {
+    color: #dc3545;
+    text-decoration: none;
+    background-color: transparent;
+}
+
+
+.pulsate {
+    -webkit-animation: pulsate 1s ease-out;
+    -webkit-animation-iteration-count: infinite; 
+    opacity: 0.5;
+}
+@-webkit-keyframes pulsate {
+    0% { 
+        opacity: 0.5;
+    }
+    50% { 
+        opacity: 1.0;
+    }
+    100% { 
+        opacity: 0.5;
+    }
+
+
+</style>
+
+
+</html>
