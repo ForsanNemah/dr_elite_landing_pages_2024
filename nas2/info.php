6392165af17f268bf2400d90e43620836d5acad2
ForsanNemah <forsan20172017@gmail.com>
2024-03-17 06:59:40 +0300

    aa

---
+++ nas2/info.php
@@ -0,0 +1,295 @@
+<?php
+error_reporting(E_ERROR | E_PARSE);
+
+ 
+//$ad_source=echo "<script> alert(""); </script>";
+
+ 
+$action_url="https://script.google.com/macros/s/AKfycbxWvDNwtE6X1-JHRBpZVB5t1nC1Hk84_Vl84yAjjlJHVmFhsLD_4iJ3TLe311BK3dOZ7g/exec";
+
+$logo_h='120px';
+$logo_w='120px';
+$address=" 
+
+
+ 
+
+";
+$email="info@wmc-ksa.com";
+$form_bg_color="#8C730C";
+$bg_image="asnan.jpg";
+$sliders=0;
+$add_source="snap 1  ";
+$phn="966552557304";
+$phn2="966564756555";
+$phn0="966564756555";
+$end_date="20-3-2023";
+$end_time="12:00";
+$insta_url="https://www.instagram.com/dr.kareem_asran/";
+$snap_url="https://www.instagram.com/dr.eman_elsharkawy/?hl=ar";
+$gmap_url="https://goo.gl/maps/S1Zko9MvKiYzqPQV9";
+
+$site_name="     مجمع ناس2        ";
+$dr_name=
+"
+ 
+";
+$add_title="50% خصومات تصل الى ";
+
+$sidebar1_msg="نحلم ونحقق";
+$sidebar2_msg="خدماتنا ";
+$sidebar3_msg="خدماتنا ";
+
+
+
+$full_map_url='
+
+
+
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3621.7562572822294!2d46.8625626!3d24.8037985!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e2faad7b730cee5%3A0x7435f12a419a2290!2z2YXYsdmD2LIg2LnZitin2K_Yp9iqINmC2LXYsSDYp9mE2KzZhdin2YQg2KfZhNi32KjZig!5e0!3m2!1sar!2ssa!4v1707776853949!5m2!1sar!2ssa" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+
+
+
+
+
+
+';
+
+
+
+
+
+
+
+
+
+
+
+
+//echo "<script> alert('".$gmap_url."'); </script>";
+
+ 
+
+
+
+
+
+try {
+
+
+
+    $items = array();
+
+
+
+$handle = fopen("services.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+    $items_branches = array();
+
+
+
+$handle = fopen("branches.txt", "r");
+if ($handle) {
+$i=0;
+    while (($line = fgets($handle)) !== false) {
+        //echo $line."<br>".$i; 
+$i++;
+$items_branches['Peter'.$i]=$line;
+
+
+    }
+
+    fclose($handle);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+$info = array(
+
+    
+
+    "website_title"=>$site_name,
+     "sub1"=>"      الحمزة للسيارات  ",
+    "sub2"=>"أفضل عروض شانجان - شانجان جديدة بأفضل سعر   
+      ",
+
+    "counter_message"=>"     احجز قبل انتهاء العداد للحصول على العروض          ",
+
+
+
+ "sidebar1_msg"=>$sidebar1_msg,
+ "sidebar2_msg"=>$sidebar2_msg,
+ "sidebar3_msg"=>$sidebar3_msg,
+
+    "form_sub1"=>"               ",
+    "form_sub2"=>"      ",
+
+
+ 
+    "ev_logo_name"=>
+    $site_name
+    ,
+
+
+
+    
+    "ev_form_sub1"=>"    
+    املأ النموذج الآن
+    
+    ",
+
+
+    "ev_form_sub2"=>"    
+    
+واحصل على العروض
+    ",
+
+
+    
+    "lt_sub1"=>"    
+    
+
+ 
+    ",
+
+
+    "lt_sub2"=>"    
+
+ 
+
+    تتوفر خدمة التقسيط على دفعات
+
+
+
+ 
+    ",
+
+
+
+
+);
+
+
+//echo "Peter is " . $age['Peter'] . " years old.";
+
+
+
+
+
+
+
+
+
+
+
+
+
+$cats1 = array(
+
+    "item0"=>"نساء   ",
+
+    "item1"=>"رجال",
+ 
+
+
+
+
+
+);
+
+
+    
+  } catch(Exception $e) {
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+$website_des="
+
+
+
+
+
+ 
+
+
+";
+
+
+
+$footer_info="
+
+يتكون فريقنا من استشاريين ومتخصصين ذوي مهارات عالية يقدمون الخدمات الطبية المتاحة الأكثر تقدمًا.
+
+
+";
+
+
+$about="
+
+
+عيادات متخصصه في طب الاسنان والجراحه والتجميل
+ 
+
+
+";
+
+
+?>
---
+++ nas2/w_app_form.php
@@ -0,0 +1,138 @@
+
+
+<section id="w_app_form" class="st-shape-wrap st-gray-bg">
+      <div class="st-shape4">
+        <img src="assets/img/shape/section_shape.png" alt="shape1">
+      </div>
+      <div class="st-shape6">
+        <img src="assets/img/shape/contact-shape3.svg" alt="shape3">
+      </div>
+      <div class="st-height-b120 st-height-lg-b80"></div>
+      <div class="container">
+        <div class="st-section-heading st-style1">
+        <br>
+<br>
+
+          <h2 class="st-section-heading-title">     تواصل معنا عبر وتساب  </h2>
+
+          
+
+          <div class="st-section-heading-subtitle pulsate">
+ سجل بيانتك وسيتم تحويلك الى وتساب مباشرة
+          
+          </div>
+        </div>
+        <div class="st-height-b40 st-height-lg-b40"></div>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-10 offset-lg-1">
+            <form onsubmit="myFunction2()" method="POST" action="post_to_google_sheet.php" class="st-appointment-form form-bg"  >
+              <div id="st-alert1"></div>
+              <div class="row">
+                <div class="col-lg-6">
+                  <div class="st-form-field st-style1">
+                    <label>الاسم</label>
+                    <input type="text"   name="name" placeholder="الاسم" required>
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="st-form-field st-style1">
+                    <label>رقم الجوال</label>
+                    <input id="phn_form2" type="number"   name="phone" placeholder="555444333" required>
+                  </div>
+                </div>
+                
+
+                <input hidden type="number"   name="is_w_app" value="1" >
+
+               
+
+<?php
+
+include "services_select.php";
+?>
+
+
+
+               
+                
+               
+                
+                <div class="col-lg-12">
+
+
+              
+      
+   
+
+
+               
+                  <button  id="save_button_two" class="st-btn st-style1 st-color1 st-size-medium mybutton" type="submit" id=""
+                    name="submit">فتح وتساب</button>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+
+
+
+    <script>
+function myFunction2() {
+  //alert("The form was submitted");
+  
+   
+
+  document.getElementById("save_button_two").disabled = true;
+  document.getElementById('save_button_two').innerText = 'يرجى الانتظار سيتم تحويلك الان';
+
+
+
+
+
+
+
+
+
+
+
+
+
+  var event_phn=document.getElementById('phn_form2').value
+ 
+
+  snaptr('track','SIGN_UP', {
+  
+  'user_phone_number': sha256("966"+event_phn)          
+});
+
+
+
+
+
+
+
+
+
+
+
+ttq.identify({
+
+"phone_number":sha256("966"+event_phn)  // string. The phone number of the customer if available. It must be hashed with SHA-256 on the client side.
+
+});
+
+
+ttq.track('CompleteRegistration', {});
+
+
+
+
+}
+</script>
