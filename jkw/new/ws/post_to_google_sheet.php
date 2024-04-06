7e576b7a9e950b63de24b6fbd2c3f7dd2809e8f8
ForsanNemah <forsan20172017@gmail.com>
2024-04-06 14:20:13 +0300

    gg

--- jkw/new/ws/post_to_google_sheet.php
+++ jkw/new/ws/post_to_google_sheet.php
@@ -127,28 +127,7 @@ echo "w_api start 2";
 
 
 
-
-$curl = curl_init();
-
-curl_setopt_array($curl, array(
-  CURLOPT_URL => '185.182.186.101:2000/send-text',
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_POSTFIELDS => 'token=2000&phn=966568430828&msg='.$msg,
-  CURLOPT_HTTPHEADER => array(
-    'Content-Type: application/x-www-form-urlencoded'
-  ),
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-echo $response;
+ 
 
 
 
@@ -189,13 +168,15 @@ echo $response;
 
     $postParameter="phn=".$phone."&msg=".$msg."&token=".$token;
     
-    $curlHandle = curl_init("http://elcuzn1.com/send-text-group");
+    $curlHandle = curl_init("http://alamerms.com/send-text-group");
     curl_setopt($curlHandle, CURLOPT_POST, true);
     curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
     curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $postParameter);
     curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER,);
+    curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER,false);
     curl_setopt($curlHandle, CURLOPT_PORT, 2000);
+    curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 0); 
+
     $curlResponse = curl_exec($curlHandle);
     echo $curlResponse."res";
     curl_close($curlHandle);
