cdc4156308e640f9f5e84498f54b9aad8fe5ad98
ForsanNemah <forsan20172017@gmail.com>
2024-04-21 17:21:40 +0300

    سسس

--- meswakg/w_app_form.php
+++ meswakg/w_app_form.php
@@ -4,8 +4,7 @@
       <div class="card mt-5">
         <div class="card-body" style="background-color: green;">
           <h2 class="card-title text-center mb-4" style="color: white;">
-            احجز الحين مكانك في المسواك
-            لأقرب مكان لك
+           قم بتعبئة الفورم وسيتم تحويلك الى واتساب مباشرة
           </h2>
           <form method="post" action="post_to_google_sheet.php" onsubmit="form2_action()">
 
