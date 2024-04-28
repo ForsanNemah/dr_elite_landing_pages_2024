b166181e8873c5c1f38da3283d14c428d6c5e3c6
ForsanNemah <forsan20172017@gmail.com>
2024-04-28 15:01:15 +0300

    sss

--- meswak_group/meswak/Rafha/dental/snap/info.php
+++ meswak_group/meswak/Rafha/dental/snap/info.php
@@ -7,7 +7,7 @@ $website_name="
 ";
 
 
-$phone_main="966568430828";
+$phone_main="966541000065";
 
 $action_url="https://script.google.com/macros/s/AKfycbwcllES9ESiuMwNBkSgB4tTdWY6vHjG2UAAvcX_kP_p28bm5bnAaa1gJr9oDu-l9ACKiw/exec";
 
