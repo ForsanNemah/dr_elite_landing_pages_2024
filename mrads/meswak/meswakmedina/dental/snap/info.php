7f49632a3031abb45daa60e44ea9b3cfd1cb4433
ForsanNemah <forsan20172017@gmail.com>
2024-04-30 16:47:14 +0300

    aa

---
+++ mrads/meswak/meswakmedina/dental/snap/info.php
@@ -0,0 +1,97 @@
+<?php
+
+
+$website_name="
+مجمع المسواك الطبي 
+
+";
+
+
+$phone_main="966599955957";
+
+$action_url="https://script.google.com/macros/s/AKfycbz4KtcWxfRGNfQckFrINMeKGbomuvab24AyXtR17gcz4VrtTbWxlOfGQnPEC6HuPZc/exec";
+
+
+$form1_des="
+احجز الان
+";
+
+
+$form1_des2="
+
+
+وسيتم التواصل معك فورا
+
+";
+
+
+$form2_des="
+
+قم بتعبئة الفورمة
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+مجمع المسواك الطبي 
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+ليزر-جلدية-اسنان
+
+";
+
+
+
+$footer_des4="
+
+من 9 صباحاً الى 12 ظهراً
+من 4 مساءً الى 9 مساءً
+ 
+
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$services=1;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$w_app_button=1;
+$api_notification=1;
+$ad_source="snap ";
+
+
+
+?>
