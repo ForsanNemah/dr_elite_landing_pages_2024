e071afc02c5c750ebcd4e8259078c340b738ab42
ForsanNemah <forsan20172017@gmail.com>
2024-04-30 22:13:46 +0300

    a

---
+++ mrads/royal/royalsheraton-dammam/derma/snap/info.php
@@ -0,0 +1,97 @@
+<?php
+
+
+$website_name="
+مجمع رويال الطبي 
+
+";
+
+
+$phone_main="966541000065";
+
+$action_url="https://script.google.com/macros/s/AKfycbwAWwJm1kFn53KoYm6Nr3OEVTnlpRUz1uunbVXL0Rh2kUrpglsp_jwCEOaBuopmXKhY/exec";
+
+
+$form1_des="
+احجز الان
+";
+
+
+$form1_des2="
+
+
+وسيتم التواصل معك فورا
+
+";
+
+
+$form2_des="
+
+قم بتعبئة الفورمة
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+مجمع رويال الطبي 
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+ليزر-جلدية-اسنان
+
+";
+
+
+
+$footer_des4="
+
+من 9 صباحاً الى 12 ظهراً
+من 4 مساءً الى 9 مساءً
+ 
+
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$w_app_button=0;
+$api_notification=0;
+
+
+
+?>
