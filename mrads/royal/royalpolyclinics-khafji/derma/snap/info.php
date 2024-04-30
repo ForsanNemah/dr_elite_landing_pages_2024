26b70af4f2356f8104bbdc61899c7c448119d1c1
ForsanNemah <forsan20172017@gmail.com>
2024-04-30 22:28:18 +0300

    a

---
+++ mrads/royal/royalpolyclinics-khafji/derma/snap/info.php
@@ -0,0 +1,98 @@
+<?php
+
+
+$website_name="
+مجمع رويال الطبي 
+
+";
+
+
+$phone_main="966536332299";
+
+$action_url="https://script.google.com/macros/s/AKfycbz8lb-Gc_MO08n11BK4qzuJdMdLdHN0-rDNIY4Bp3F-sdnOIRQxoOrKgLG5JOhpH-Nh/exec";
+
+
+$form1_des="
+احجز الان
+";
+
+
+$form1_des2="
+
+
+وسيتم التواصل معك فورا
+
+";
+
+
+$form2_des="
+
+قم بتعبئة الفورمة
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+مجمع رويال الطبي 
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+ليزر-جلدية-اسنان
+
+";
+
+
+
+$footer_des4="
+
+من 9 صباحاً الى 12 ظهراً
+من 4 مساءً الى 9 مساءً
+ 
+
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$w_app_button=0;
+$api_notification=0;
+$ad_source="snap ";
+
+
+
+?>
