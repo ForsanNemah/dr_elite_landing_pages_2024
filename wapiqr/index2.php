f21e1d15edbadb1ca90c29d98ec67f3f6acb58f3
ForsanNemah <forsan20172017@gmail.com>
2024-05-05 12:42:12 +0300

    aa

---
+++ wapiqr/index2.php
@@ -0,0 +1,14 @@
+<?php
+$url = 'http://185.182.186.101:3000';
+
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+
+$response = curl_exec($curl);
+curl_close($curl);
+
+echo $response;
+?>
