18c64ee85d559e25cfae0ace9714b5c7e4423874
ForsanNemah <forsan20172017@gmail.com>
2024-05-11 13:34:41 +0300

    ss

--- mrads/meswak/hafaralbatin-ashraqa/dental/snap/head.php
+++ mrads/meswak/hafaralbatin-ashraqa/dental/snap/head.php
@@ -77,7 +77,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
+snaptr('init', '273696fa-d5e8-42c4-aee2-974a08581ed7');
 
 snaptr('track', 'PAGE_VIEW');
  
