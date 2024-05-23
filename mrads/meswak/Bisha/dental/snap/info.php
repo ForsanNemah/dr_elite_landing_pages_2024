00650b25cb7a2fd5e7022ebef8dd6ecdd3380dae
ForsanNemah <forsan20172017@gmail.com>
2024-05-23 15:09:49 +0300

    aa

--- mrads/meswak/Bisha/dental/snap/info.php
+++ mrads/meswak/Bisha/dental/snap/info.php
@@ -7,7 +7,7 @@ $website_name="
 ";
 
 
-$phone_main="966595776790";
+$phone_main="966555069509";
 
 $action_url="https://script.google.com/macros/s/AKfycbzprbbpUyRBqxLkvnZd5Xh6FmO_edIN6rsRx3Iw5KRn8hEMe5Xn_1E54ZVlP_doosSU/exec";
 
@@ -88,8 +88,8 @@ $faq=0;
 $process=0;
 $services_images=0;
 $footer=1;
-$w_app_button=0;
-$api_notification=0;
+$w_app_button=1;
+$api_notification=1;
 $ad_source="snap ";
 
 
--- mrads/meswak/tabuk2/dental/snap/info.php
+++ mrads/meswak/tabuk2/dental/snap/info.php
@@ -7,7 +7,7 @@ $website_name="
 ";
 
 
-$phone_main="966595776790";
+$phone_main="966580000239";
 
 $action_url="https://script.google.com/macros/s/AKfycbxk7DODCo1tj5JGB4lZVUcGWa5zklW2gSBDBXms7A191KfhGyT8YjAUwF8ZR4yBgXTAag/exec";
 
@@ -88,8 +88,8 @@ $faq=0;
 $process=0;
 $services_images=0;
 $footer=1;
-$w_app_button=0;
-$api_notification=0;
+$w_app_button=1;
+$api_notification=1;
 $ad_source="snap ";
 
 
