bac95b5cc30b7cbc2a51e8fdcc552a8c71d82f0e
ForsanNemah <forsan20172017@gmail.com>
2024-05-23 15:49:24 +0300

    aa

--- mrads/royal/alqods/derma/snap/info.php
+++ mrads/royal/alqods/derma/snap/info.php
@@ -7,7 +7,7 @@ $website_name="
 ";
 
 
-$phone_main="966541000065";
+$phone_main="966566112241";
 
 $action_url="https://script.google.com/macros/s/AKfycbycvAFLZiM5noovBE4BzwULBaPPhyz5xfNO8PkPpQ0JpofH_99BcDdTy_9O8rqPLzIj3g/exec";
 
@@ -89,8 +89,8 @@ $faq=0;
 $process=0;
 $services_images=0;
 $footer=1;
-$w_app_button=0;
-$api_notification=0;
+$w_app_button=1;
+$api_notification=1;
 
 
 
