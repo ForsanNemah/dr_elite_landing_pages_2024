03b00c11a4c637856f24e194bcfaccf43e9aa3c8
ForsanNemah <forsan20172017@gmail.com>
2024-05-28 04:49:08 +0300

    aa

---
+++ 2moh_task/form.php
@@ -0,0 +1,403 @@
+<div class="form-container" id="myform">
+  <div class="row justify-content-center custom_row_margin">
+    <div class="col-lg-6">
+      <div class="card mt-5">
+        <div class="card-body form_bg" >
+          <h2 class="card-title text-center mb-4" style="color: white;">
+          <?php
+          echo $form1_des;
+          ?>
+<br>
+
+<?php
+          echo $form1_des2;
+          ?>
+
+
+
+          </h2>
+          <form method="post" action="post_to_google_sheet.php" onsubmit="form1_action()">
+
+
+
+
+          <input type="text" class="form-control" name="source" value="<?php echo $ad_source ?>"  hidden>
+
+          <input type="text" class="form-control" name="is_w_app" value="0"  hidden>
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="mb-3">
+              
+              <input type="text" class="form-control" name="name" placeholder="عنوان المهمة" required>
+            </div>
+
+
+
+
+            <div class="mb-3">
+              
+
+
+            <textarea class="form-control"  name="des" rows="4" cols="50" placeholder=" وصف المهمة" >وصف المهمة 
+</textarea>
+ 
+ 
+            </div>
+
+
+
+
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="mb-3">
+              
+              <input type="text" class="form-control" name="files_url" placeholder="رابط ملفات المهمة (اختياري)"  >
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="mb-3" hidden>
+
+
+              <select name="branch"    class="form-select">
+
+
+  <?php
+    $filename = 'branches.txt'; // Specify the path to your text file
+
+    // Check if the file exists
+    if (file_exists($filename)) {
+        // Read the file into an array, with each line as an element
+        $lines = file($filename, FILE_IGNORE_NEW_LINES);
+
+        // Output the HTML select tag
+        
+
+        // Iterate over the lines and create an option for each line
+        foreach ($lines as $line) {
+            echo '<option>' . htmlspecialchars($line) . '</option>';
+        }
+
+        // Close the HTML select tag
+        
+    } else {
+        echo 'File not found.';
+    }
+?>
+
+</select>
+
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="mb-3">
+
+
+<select name="task_sender_name"    class="form-select">
+
+
+<?php
+$filename = 'services2.txt'; // Specify the path to your text file
+
+// Check if the file exists
+if (file_exists($filename)) {
+// Read the file into an array, with each line as an element
+$lines = file($filename, FILE_IGNORE_NEW_LINES);
+
+// Output the HTML select tag
+
+
+// Iterate over the lines and create an option for each line
+foreach ($lines as $line) {
+echo '<option>' . htmlspecialchars($line) . '</option>';
+}
+
+// Close the HTML select tag
+
+} else {
+echo 'File not found.';
+}
+?>
+
+</select>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            <div class="mb-3">
+
+
+<select name="emp_name"    class="form-select">
+
+
+<?php
+$filename = 'services.txt'; // Specify the path to your text file
+
+// Check if the file exists
+if (file_exists($filename)) {
+// Read the file into an array, with each line as an element
+$lines = file($filename, FILE_IGNORE_NEW_LINES);
+
+// Output the HTML select tag
+
+
+// Iterate over the lines and create an option for each line
+foreach ($lines as $line) {
+echo '<option>' . htmlspecialchars($line) . '</option>';
+}
+
+// Close the HTML select tag
+
+} else {
+echo 'File not found.';
+}
+?>
+
+</select>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+
+
+
+
+            <div class="text-center">
+
+
+            <img  class="img-fluid" id="loading_gif" src="images/loading.gif" alt="Italian Trulli"  width="50" height="50" >
+
+
+              <button id="form1_button" type="submit" class="btn btn-primary btn-form rounded-pill">ارسال </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+<div class="container mt-4"  >
+  <div class="row justify-content-center">
+    <div class="col-lg-8 text-center">
+      <h1>
+    
+    
+<a href="https://docs.google.com/spreadsheets/d/1OVDF84OrYyR1cY2Y4ARp8eKJfcD9cCMvZvmaiyopmII/edit?usp=sharing">فتح شيت المهام </a>
+    
+    
+    
+    </h1>
+      <div class="accordion"  >
+        <!-- Accordion items go here -->
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+
+
+$('#loading_gif').hide();
+
+
+function form1_action() {
+  //alert("The form was submitted");
+  
+ 
+  document.getElementById("form1_button").disabled = true;
+ 
+
+  //$('#form1_button').hide();
+  //$('#loading_gif').show();
+ 
+  //alert("wwe");
+  document.getElementById('form1_button').innerText = 'يرجى الإنتظار سيتم تحويلك الان';
+
+
+
+
+
+
+
+
+  var event_phn=document.getElementById('phn_form1').value
+ 
+
+  snaptr('track','SIGN_UP', {
+  
+    'user_phone_number': sha256("966"+event_phn)          
+});
+
+
+
+
+
+
+
+
+
+
+
+ttq.identify({
+ 
+	"phone_number":sha256("966"+event_phn)  // string. The phone number of the customer if available. It must be hashed with SHA-256 on the client side.
+ 
+});
+
+
+ttq.track('CompleteRegistration', {});
+
+
+
+
+
+//alert("submited"+event_phn);
+
+
+
+
+
+}
+</script>
---
+++ 2moh_task/post_to_google_sheet.php
@@ -0,0 +1,397 @@
+<?php
+
+error_reporting(E_ERROR | E_PARSE);
+ini_set('max_execution_time', '0');
+include "info.php";
+
+$name=$_POST['name'];
+$des=$_POST['des'];
+$files_url=$_POST['files_url'];
+$is_w_app=$_POST['is_w_app'];
+$source=$_POST['source'];
+$task_sender_name=$_POST['task_sender_name'];
+$emp_name=$_POST['emp_name'];
+$branch=$_POST['branch'];
+
+//$source="snap 1";
+
+date_default_timezone_set("Asia/Riyadh");
+$date = date('Y/m/d H:i:s');
+
+
+
+$whatsapp_link="wa.me/966".$phone;
+echo $name.$phone.$is_w_app;
+
+
+
+
+$postParameter = array(
+    'date_and_time' => $date,
+    'name' => $name,
+    'des' => $des,
+    'files_url' => $files_url,
+    'whatsapp_link' => $whatsapp_link,
+    'task_sender_name' => $task_sender_name,
+    'emp_name' => $emp_name,
+    'branch' => $branch,
+    'source' => $source
+    
+);
+
+
+
+
+
+
+//echo "w_api start";
+
+
+
+if(1==1){
+
+    $action_url="https://script.google.com/macros/s/AKfycbx5AQiVvF8GlzMv6QQFh1_HlxvA2b5Egh4_lpYTG9vCrFCf_zdAxPGnrISYZPAZjhDl/exec";
+        
+    $curlHandle = curl_init($action_url);
+    curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $postParameter);
+    curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, false);
+    $curlResponse = curl_exec($curlHandle);
+    curl_close($curlHandle);
+
+
+
+
+
+
+
+
+    $w_app_msg1=" مهمة جديدة  بعنوان  ".":".$name."\n";
+    $w_app_msg2=$w_app_msg1." مسند المهمة"." :".$task_sender_name."\n";
+    $w_app_msg3=$w_app_msg2." مستلم المهمة".":".$emp_name."\n";
+    $w_app_msg4=$w_app_msg3."وصف المهمة".":".$des."\n";
+    
+    send_w_app_msg_groups("120363212806523669",$w_app_msg4,"2000");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    echo '<script type="text/javascript">
+
+    var msg="تمت العملية بنجاح ";   
+ 
+ 
+ 
+           
+    
+
+
+
+
+ alert(msg);
+ 
+ //window.history.go(-1);
+ 
+ window.history.go(-1);
+ 
+ //window.location.replace("https://almeswak.ksa-ads.com/34laban/dental/snap/done.php");
+ 
+ </script>
+ 
+ ';
+
+
+
+
+
+
+}
+else{
+
+    echo " not mohaned";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if($api_notification==1){
+        
+    $w_app_msg1="  لديك تسجيل  جديد  باسم  ".$name;
+    $w_app_msg2=$w_app_msg1." برقم جوال ".$phone;
+    
+    send_w_app_msg($phone_main,$w_app_msg2,"2000");
+
+}
+
+
+
+//echo $curlResponse."";
+
+
+
+
+
+
+
+
+
+if($is_w_app=="1"){
+
+    if($api_notification==1){
+        
+        $w_app_msg1="  لديك تسجيل  جديد  باسم  ".$name;
+        $w_app_msg2=$w_app_msg1." برقم جوال ".$phone;
+        
+        send_w_app_msg($phone_main,$w_app_msg2,"2000");
+    
+    }
+
+    header("Location:https://wa.me/".$phone_main);
+
+}
+
+else{
+
+
+
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function send_w_app_msg($phone,$msg,$token) {
+
+
+    
+
+echo "w_api start 2";
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//echo $phone.$msg.$token;
+
+
+
+/*
+    $postParameter = array(
+        
+        
+        'phn' => $phone,
+        'token' => $token,
+        'msg' => $msg
+        
+    );
+
+    */
+
+
+    $postParameter="phn=".$phone."&msg=".$msg."&token=".$token;
+    
+    $curlHandle = curl_init("http://alamerms.com/send-text");
+    curl_setopt($curlHandle, CURLOPT_POST, true);
+    curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+    curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $postParameter);
+    curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER,false);
+    curl_setopt($curlHandle, CURLOPT_PORT, 2000);
+    curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 0); 
+
+    $curlResponse = curl_exec($curlHandle);
+    echo $curlResponse."res";
+    curl_close($curlHandle);
+
+
+    //print_r(curl_getinfo($curlHandle));
+
+    if(curl_errno($curlHandle)){
+        echo 'Curl error: ' . curl_error($curlHandle);
+    }
+    
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  function send_w_app_msg_groups($phone,$msg,$token) {
+
+
+    
+
+    echo "w_api start 2";
+    
+    
+    
+    
+     
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //echo $phone.$msg.$token;
+    
+    
+    
+    /*
+        $postParameter = array(
+            
+            
+            'phn' => $phone,
+            'token' => $token,
+            'msg' => $msg
+            
+        );
+    
+        */
+    
+    
+        $postParameter="phn=".$phone."&msg=".$msg."&token=".$token;
+        
+        $curlHandle = curl_init("http://alamerms.com/send-text-group");
+        curl_setopt($curlHandle, CURLOPT_POST, true);
+        curl_setopt($curlHandle, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+        curl_setopt($curlHandle, CURLOPT_POSTFIELDS, $postParameter);
+        curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER,false);
+        curl_setopt($curlHandle, CURLOPT_PORT, 2000);
+        curl_setopt($curlHandle, CURLOPT_CONNECTTIMEOUT, 0); 
+    
+        $curlResponse = curl_exec($curlHandle);
+        echo $curlResponse."res";
+        curl_close($curlHandle);
+    
+    
+        //print_r(curl_getinfo($curlHandle));
+    
+        if(curl_errno($curlHandle)){
+            echo 'Curl error: ' . curl_error($curlHandle);
+        }
+        
+      }
+
+
+
+
+
+
+
+?>
