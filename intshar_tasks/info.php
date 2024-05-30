5a043cb6e78577128d942fa83d2ff51a9849cba8
ForsanNemah <forsan20172017@gmail.com>
2024-05-30 03:28:03 +0300

    aa

---
+++ intshar_tasks/info.php
@@ -0,0 +1,97 @@
+<?php
+
+
+$website_name="
+مجمع المسواك الطبي 
+
+";
+
+
+$phone_main="966595776790";
+
+
+
+$form1_des="
+
+اكتب مهمتك  وصفها بدقة 
+";
+
+
+$form1_des2="
+
+
+ 
+
+";
+
+
+$form2_des="
+
+قم بتعبئة الفورمة
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+مجمع المسواك الطبي 
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+ليزر-جلدية-اسنان
+
+";
+
+
+
+$footer_des4="
+
+من 9 صباحاً الى 12 ظهراً
+من 4 مساءً الى 9 مساءً
+ 
+
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$w_app_button=0;
+$api_notification=0;
+$ad_source="snap ";
+
+
+
+?>
