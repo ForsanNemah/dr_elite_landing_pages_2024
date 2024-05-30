616da9eec66bbed06c0720c64135a9ca860a837f
ForsanNemah <forsan20172017@gmail.com>
2024-05-30 11:30:52 +0300

    aa

--- dr_noha/head.php
+++ dr_noha/head.php
@@ -78,7 +78,7 @@ r.src=n;var u=t.getElementsByTagName(s)[0];
 u.parentNode.insertBefore(r,u);})(window,document,
 'https://sc-static.net/scevent.min.js');
 
-snaptr('init', '31104507-bf96-4c29-a273-a1bad59360d8');
+snaptr('init', '77ec2e1e-c2bb-468f-8d2e-f99b7ec9983c');
 
 snaptr('track', 'PAGE_VIEW');
  
