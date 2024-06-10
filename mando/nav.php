28014054caee2882c731c51b28ef14550dbcb201
ForsanNemah <forsan20172017@gmail.com>
2024-06-10 13:50:18 +0300

    aa

--- mando/nav.php
+++ mando/nav.php
@@ -11,7 +11,7 @@
   }
 </style>
 
-<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top mb-4">
   <div class="container">
     <a class="navbar-brand" href="#"> مركز الهجن  </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
