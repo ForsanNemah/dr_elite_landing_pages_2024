40cb2bf959d8aa526f9894bbedba8c4998072302
ForsanNemah <forsan20172017@gmail.com>
2024-06-11 11:28:58 +0300

    aa

---
+++ tasks_api/2moh/mohaned.php
@@ -0,0 +1,19 @@
+<?php
+$url = 'https://opensheet.elk.sh/1OVDF84OrYyR1cY2Y4ARp8eKJfcD9cCMvZvmaiyopmII/Sheet1';
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+
+$response = curl_exec($ch);
+
+if ($response === false) {
+    echo 'Error: ' . curl_error($ch);
+} else {
+    print_r($response);
+}
+
+curl_close($ch);
+?>
