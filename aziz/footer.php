e363461d3df1e8ff69d11f0372d0ec7fa298bce0
ForsanNemah <forsan20172017@gmail.com>
2024-06-19 23:34:39 +0300

    aa

---
+++ aziz/footer.php
@@ -0,0 +1,35 @@
+
+    <!-- Footer -->
+    <footer class="page-footer font-small blue">
+    
+<!-- Copyright -->
+ 
+
+<div class="footer-copyright text-center py-3">    
+   
+ 
+ 
+
+   
+</div>
+<!-- Copyright -->
+
+
+<div class="footer-copyright text-center py-3">    
+ 
+ 
+ <a style="color:blue;" href="privicy.php">سياسة الخصوصية  </a>
+
+   
+</div>
+
+
+<div class="footer-copyright text-center py-3">    
+ 
+ 
+ <a style="color:blue;" href="https://intshar.net"> برمجة</a>
+
+   
+</div>
+
+</footer>
---
+++ aziz/youtube_video.php
@@ -0,0 +1,5 @@
+<div id="youtube_video">
+
+<iframe width="80%" height="315" src="https://www.youtube.com/embed/F3ZgLlIg04Q?si=VbJJFUwgV-e7K5lF" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+</div>
