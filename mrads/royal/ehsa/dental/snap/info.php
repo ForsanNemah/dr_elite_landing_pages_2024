2c4e38edae7934771f77d2f2eecf754f5645d7c8
ForsanNemah <forsan20172017@gmail.com>
2024-06-24 18:38:33 +0300

    aa

---
+++ mrads/royal/ehsa/dental/snap/info.php
@@ -0,0 +1,98 @@
+<?php
+
+
+$website_name="
+مجمع رويال الطبي 
+
+";
+
+
+$phone_main="966543445556";
+
+$action_url="https://script.google.com/macros/s/AKfycbxu7dHSMGkI8ScOFV0mlKvSY4iMTpBPRoJMbkI8WqjAUW02SghAmuPxB0szb02O4aL-fQ/exec";
+
+
+$form1_des="
+احجز الان
+";
+
+
+$form1_des2="
+
+
+وسيتم التواصل معك فورا
+
+";
+
+
+$form2_des="
+
+قم بتعبئة الفورمة
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+مجمع رويال الطبي 
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+ليزر-جلدية-اسنان
+
+";
+
+
+
+$footer_des4="
+
+من 9 صباحاً الى 12 ظهراً
+من 4 مساءً الى 9 مساءً
+ 
+
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$w_app_button=1;
+$api_notification=1;
+$ad_source="snap ";
+
+
+
+?>
