8d30ed0738034b160af3d1f31e9dc82e7e132645
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-06-27 10:51:34 +0300

    ss

---
+++ mrads/royal/roda/derma/snap/info.php
@@ -0,0 +1,97 @@
+<?php
+
+
+$website_name="
+مجمع رويال الطبي 
+
+";
+
+
+$phone_main="966566112241";
+
+$action_url="https://script.google.com/macros/s/AKfycbyp4WkD5dSonFKj87KMgnoKksIQh_vWXMCWbcto5fSmb-oQtn1TTesHoKG5ebFf1jCg8w/exec";
+
+
+$form1_des="
+احجز الان
+";
+
+
+$form1_des2="
+
+
+وسيتم التواصل معك فورا
+
+";
+
+
+$form2_des="
+
+قم بتعبئة الفورمة
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+مجمع رويال الطبي 
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+ليزر-جلدية-اسنان
+
+";
+
+
+
+$footer_des4="
+
+من 9 صباحاً الى 12 ظهراً
+من 4 مساءً الى 9 مساءً
+ 
+
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$w_app_button=1;
+$api_notification=1;
+
+
+
+?>
