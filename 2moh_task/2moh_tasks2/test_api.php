cfc20cfc624cac0e7f26677677e6ae61cf4a5d0c
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-06-30 18:50:41 +0300

    aa

--- 2moh_task/2moh_tasks2/test_api.php
+++ 2moh_task/2moh_tasks2/test_api.php
@@ -15,44 +15,46 @@ if ($result['status_code'] == 200) {
 
 
 
+function send_to_wapi($auth, $profileId, $phone, $message) {
+    $curl = curl_init();
 
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'POST',
+        CURLOPT_POSTFIELDS => http_build_query(array(
+            'auth' => $auth,
+            'profile_id' => $profileId,
+            'phone' => $phone,
+            'msg' => $message
+        )),
+        CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/x-www-form-urlencoded',
+            'Authorization: ' . $auth
+        ),
+    ));
 
-function send_to_wapi($auth, $profile_id, $phone, $msg) {
-    try {
-        $url = "https://ads.2moh.net/wapi2024/?auth=$auth&profile_id=$profile_id&phone=$phone&msg=" . urlencode($msg);
+    $response = curl_exec($curl);
 
-        $curl = curl_init();
-        
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-        ));
-        
-        $response = curl_exec($curl);
-        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        
+    if (curl_errno($curl)) {
+        $error = curl_error($curl);
         curl_close($curl);
-        
-        return array(
-            'status_code' => $httpCode,
-            'response' => $response
-        );
-    } catch (Exception $e) {
-        return array(
-            'status_code' => 500,
-            'response' => 'Error: ' . $e->getMessage()
-        );
+        return array('error' => $error);
     }
+
+    $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    curl_close($curl);
+
+    return array(
+        'status_code' => $statusCode,
+        'response' => $response
+    );
 }
-
-
-
 
 
 
