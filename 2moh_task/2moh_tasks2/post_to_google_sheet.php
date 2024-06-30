890bd42af017b980f3cabe055facf889f8f9b29c
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-06-30 18:29:42 +0300

    aa

--- 2moh_task/2moh_tasks2/post_to_google_sheet.php
+++ 2moh_task/2moh_tasks2/post_to_google_sheet.php
@@ -455,45 +455,50 @@ echo "w_api start 2";
 
 
     function send_with_wapi($auth, $profileId, $phone, $message) {
+ 
+
 
         $curl = curl_init();
-    
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'POST',
-            CURLOPT_POSTFIELDS => http_build_query([
-                'auth' => $auth,
-                'profile_id' => $profileId,
-                'phone' => $phone,
-                'msg' => $message
-            ]),
-            CURLOPT_HTTPHEADER => array(
-                'Content-Type: application/x-www-form-urlencoded',
-                'Authorization: ' . $auth
-            ),
-        ));
-    
-        $response = curl_exec($curl);
-    
-        if ($response === false) {
-            $error = curl_error($curl);
-            curl_close($curl);
-            return $error;
-        }
-    
-        $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-        curl_close($curl);
-    
-        return [
-            'status_code' => $statusCode,
-            'response' => $response
-        ];
+
+$data = array(
+    'auth' => $auth,
+    'profile_id' => $profileId,
+    'phone' => $phone,
+    'msg' => $message
+);
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'POST',
+    CURLOPT_POSTFIELDS => json_encode($data),
+    CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/json',
+        'Authorization: ' . $auth
+    ),
+));
+
+$response = curl_exec($curl);
+
+if ($response === false) {
+    $error = curl_error($curl);
+    curl_close($curl);
+    return $error;
+}
+
+$statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+curl_close($curl);
+
+return [
+    'status_code' => $statusCode,
+    'response' => $response
+];
+
     }
     
     
