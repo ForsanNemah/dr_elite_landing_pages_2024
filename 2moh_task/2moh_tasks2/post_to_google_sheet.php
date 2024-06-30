f1a92da6b7db51652b189670c81dea9eeb809610
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-06-30 18:34:00 +0300

    a

--- 2moh_task/2moh_tasks2/post_to_google_sheet.php
+++ 2moh_task/2moh_tasks2/post_to_google_sheet.php
@@ -77,14 +77,14 @@ if(1==1){
 
 
 
+ 
 
+    $w_app_msg1=" مهمة جديدة  بعنوان  ".":".$name."\\n";
 
-    $w_app_msg1=" مهمة جديدة  بعنوان  ".":".$name."'\n'";
-
-    $w_app_msg2=$w_app_msg1." مسند المهمة"." :".$userInfo_sender['name']."'\n'";
-    $w_app_msg3=$w_app_msg2." مستلم المهمة".":".$userInfo_reciver['name']."'\n'";
-    $w_app_msg4=$w_app_msg3."وصف المهمة".":".$des."'\n'";
-    $w_app_msg5=$w_app_msg4."  الزمن الازم" .":".$time." دقيقة"."'\n'";
+    $w_app_msg2=$w_app_msg1." مسند المهمة"." :".$userInfo_sender['name']."\\n";
+    $w_app_msg3=$w_app_msg2." مستلم المهمة".":".$userInfo_reciver['name']."\\n";
+    $w_app_msg4=$w_app_msg3."وصف المهمة".":".$des."'\\n'";
+    $w_app_msg5=$w_app_msg4."  الزمن الازم" .":".$time." دقيقة"."\\n";
     
 
 
