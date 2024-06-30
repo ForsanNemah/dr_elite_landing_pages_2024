cf14f338be9be55f470b7dcd5ea8b059f05da73f
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-06-30 19:12:33 +0300

    aa

--- 2moh_task/2moh_tasks2/post_to_google_sheet.php
+++ 2moh_task/2moh_tasks2/post_to_google_sheet.php
@@ -462,33 +462,33 @@ echo "w_api start 2";
        // echo "". $auth ."". $profileId ."". $phone.$message ;
         $curl = curl_init();
 
-$postFields = array(
-    'auth' => $auth,
-    'profile_id' => $profileId,
-    'phone' => $phone,
-    'msg' => $message
-);
+
+        
+
+        
+$curl = curl_init();
 
 curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'POST',
-    CURLOPT_POSTFIELDS => $postFields,
-    CURLOPT_HTTPHEADER => array(
-        'Content-Type: application/x-www-form-urlencoded',
-        'Authorization: ' .$auth
-    ),
+  CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'POST',
+  CURLOPT_POSTFIELDS => 'auth=40703bb7812b727ec01c24f2da518c407342559c&profile_id=aedd0dc2-8453&phone=966568430828&msg=uiop%5B\'',
+  CURLOPT_HTTPHEADER => array(
+    'Content-Type: application/x-www-form-urlencoded',
+    'Authorization: 40703bb7812b727ec01c24f2da518c407342559c'
+  ),
 ));
 
 $response = curl_exec($curl);
 
 curl_close($curl);
-return $response;
+echo $response;
+
 
 
 
