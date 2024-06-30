b9753bc63eb322be4be03b6d6ba62197b882a9f0
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-06-30 19:36:27 +0300

    aa

--- intshar_tasks_final/post_to_google_sheet.php
+++ intshar_tasks_final/post_to_google_sheet.php
@@ -92,6 +92,16 @@ if(1==1){
 
 
 
+    $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453', '120363249383561282@g.us',  $w_app_msg5);
+
+    echo print_r($result) ;
+    
+    
+    
+    $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453', $userInfo_reciver['phone']."@c.us", $w_app_msg5);
+    
+    
+    
 
 
 
@@ -419,5 +429,65 @@ echo "w_api start 2";
 
         send_w_app_msg($parts[1],$msg,"2000");
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    function send_with_wapi($auth, $profileId, $phone, $message) {
+ 
+
+
+ 
+
+
+        
+
+        
+        $curl = curl_init();
+        
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'POST',
+          CURLOPT_POSTFIELDS => 'auth='.$auth.'&profile_id='.$profileId.'&phone='.$phone.'&msg='.$message.'',
+          CURLOPT_HTTPHEADER => array(
+            'Content-Type: application/x-www-form-urlencoded',
+            'Authorization: 40703bb7812b727ec01c24f2da518c407342559c'
+          ),
+        ));
+        
+        $response = curl_exec($curl);
+        
+        curl_close($curl);
+        echo $response;
+        
+        
+        
+        
+        
+            }
 
 ?>
