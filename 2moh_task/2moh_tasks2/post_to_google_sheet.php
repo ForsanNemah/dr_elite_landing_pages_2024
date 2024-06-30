fb583671d95eb8f028a5e26eb47f095a8f300fc4
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-06-30 14:18:19 +0300

    ss

--- 2moh_task/2moh_tasks2/post_to_google_sheet.php
+++ 2moh_task/2moh_tasks2/post_to_google_sheet.php
@@ -89,31 +89,22 @@ if(1==1){
 
  
 
-$result = send_to_wapi("40703bb7812b727ec01c24f2da518c407342559c", "aedd0dc2-8453", "120363216158625125@g.us", $w_app_msg5);
-
-if ($result['status_code'] == 200) {
-    echo "Request successful. Response: " . $result['response'];
-} else {
-    echo "Request failed with status code: " . $result['status_code'] . ". Error: " . $result['response'];
-}
-
-
-
-
-
-
-
-
-$result = send_to_wapi("40703bb7812b727ec01c24f2da518c407342559c", "aedd0dc2-8453", $userInfo_reciver['phone']."@c.us", $w_app_msg5);
-
  
 
-if ($result['status_code'] == 200) {
-    echo "Request successful. Response: " . $result['response'];
-} else {
-    echo "Request failed with status code: " . $result['status_code'] . ". Error: " . $result['response'];
-}
+$result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453', '120363216158625125@g.us', $w_app_msg5);
 
+
+
+
+
+$result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453', $userInfo_reciver['phone']."@c.us", $w_app_msg5);
+
+
+
+ 
+ 
+
+ 
 
 
 
@@ -466,40 +457,45 @@ echo "w_api start 2";
 
 
 
-    function send_to_wapi($auth, $profile_id, $phone, $msg) {
-        try {
-            $url = "https://ads.2moh.net/wapi2024/?auth=$auth&profile_id=$profile_id&phone=$phone&msg=" . urlencode($msg);
+    function send_with_wapi($auth, $profileId, $phone, $message) {
+        $curl = curl_init();
     
-            $curl = curl_init();
-            
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => '',
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => 'GET',
-            ));
-            
-            $response = curl_exec($curl);
-            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-            
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'POST',
+            CURLOPT_POSTFIELDS => http_build_query([
+                'auth' => $auth,
+                'profile_id' => $profileId,
+                'phone' => $phone,
+                'msg' => $message
+            ]),
+            CURLOPT_HTTPHEADER => array(
+                'Content-Type: application/x-www-form-urlencoded',
+                'Authorization: ' . $auth
+            ),
+        ));
+    
+        $response = curl_exec($curl);
+    
+        if ($response === false) {
+            $error = curl_error($curl);
             curl_close($curl);
-            
-            return array(
-                'status_code' => $httpCode,
-                'response' => $response
-            );
-        } catch (Exception $e) {
-            return array(
-                'status_code' => 500,
-                'response' => 'Error: ' . $e->getMessage()
-            );
+            return $error;
         }
+    
+        $statusCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        curl_close($curl);
+    
+        return [
+            'status_code' => $statusCode,
+            'response' => $response
+        ];
     }
-
-
 
 ?>
