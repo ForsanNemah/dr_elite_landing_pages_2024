ec052b6340f2626af72f03f4c0cbeeb377ced6a6
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-06-30 13:34:56 +0300

    aaa

--- 2moh_task/2moh_tasks2/post_to_google_sheet.php
+++ 2moh_task/2moh_tasks2/post_to_google_sheet.php
@@ -85,8 +85,43 @@ if(1==1){
     $w_app_msg4=$w_app_msg3."وصف المهمة".":".$des."\n";
     $w_app_msg5=$w_app_msg4."  الزمن الازم" .":".$time." دقيقة"."\n";
     
-    send_w_app_msg_groups("120363216158625125",$w_app_msg5,"2000");
-    send_w_app_msg($userInfo_reciver['phone'],$w_app_msg5,"2000");
+
+
+    $auth = "40703bb7812b727ec01c24f2da518c407342559c";
+$profile_id = "aedd0dc2-8453";
+ 
+
+$result = send_to_wapi($auth, $profile_id, "120363216158625125@g.us", $w_app_msg5);
+
+if ($result['status_code'] == 200) {
+    echo "Request successful. Response: " . $result['response'];
+} else {
+    echo "Request failed with status code: " . $result['status_code'] . ". Error: " . $result['response'];
+}
+
+
+
+
+
+
+
+
+
+
+$result = send_to_wapi($auth, $profile_id, $userInfo_reciver['phone']."@c.us", $w_app_msg5);
+
+if ($result['status_code'] == 200) {
+    echo "Request successful. Response: " . $result['response'];
+} else {
+    echo "Request failed with status code: " . $result['status_code'] . ". Error: " . $result['response'];
+}
+
+
+
+
+
+    //send_w_app_msg_groups("120363216158625125",$w_app_msg5,"2000");
+    //send_w_app_msg($userInfo_reciver['phone'],$w_app_msg5,"2000");
     //split_by_hyphen($w_app_msg4);
 
 
@@ -419,5 +454,54 @@ echo "w_api start 2";
 
         send_w_app_msg($parts[1],$msg,"2000");
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    function send_to_wapi($auth, $profile_id, $phone, $msg) {
+        try {
+            $url = "https://ads.2moh.net/wapi2024/?auth=$auth&profile_id=$profile_id&phone=$phone&msg=" . urlencode($msg);
+    
+            $curl = curl_init();
+            
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => $url,
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'GET',
+            ));
+            
+            $response = curl_exec($curl);
+            $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+            
+            curl_close($curl);
+            
+            return array(
+                'status_code' => $httpCode,
+                'response' => $response
+            );
+        } catch (Exception $e) {
+            return array(
+                'status_code' => 500,
+                'response' => 'Error: ' . $e->getMessage()
+            );
+        }
+    }
+
+
 
 ?>
--- wapi2024/index.php
+++ wapi2024/index.php
@@ -1,7 +1,6 @@
 <?php
 
-
-
+/*
 
 // Check if the request method is POST
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
@@ -75,6 +74,45 @@ echo $response;
 }
 
 
+*/
+
+
+
+get_chat();
+
+
+function get_chat(){
+
+
+
+
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => 'https://wappi.pro/api/sync/chats/get?profile_id=aedd0dc2-8453&limit=3&show_all=false&offset=0',
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => '',
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 0,
+  CURLOPT_FOLLOWLOCATION => true,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => 'GET',
+  CURLOPT_HTTPHEADER => array(
+    'Authorization: 40703bb7812b727ec01c24f2da518c407342559c'
+  ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
+
+
+
+
+
+}
 
 
 
