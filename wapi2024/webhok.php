e8dfa19badab7616efd798fa5e80a83b66a9a3fe
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-07-01 13:59:55 +0300

    aa

--- wapi2024/webhok.php
+++ wapi2024/webhok.php
@@ -21,7 +21,7 @@ if(isset($event)){
 $message = $event['messages'][0]['body'];
 
     
-    $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453', '966568430828@c.us', $chat_id);
+    $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453', '966568430828@c.us', $chat_id. $message);
 
     //echo print_r($result) ;
 
