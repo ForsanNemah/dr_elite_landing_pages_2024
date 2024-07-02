1c9fade542858f58719a09240a7059695d952641
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-07-02 12:26:56 +0300

    aa

---
+++ libert/libert2/footer.php
@@ -0,0 +1,177 @@
+ 
+  <style>
+    
+  
+    
+  </style>
+</head>
+<body>
+  <footer class="footer mt-4">
+    <div class="container">
+      <div class="row">
+
+
+        <div class="col-8">
+
+        <div class="row">
+        <div class="col-8 d-flex justify-content-center">
+        <p class="text-center fs-3">   
+            
+    
+    <?php
+echo $footer_des1;
+    ?>
+    
+    
+    </p>
+         
+        
+      </div>
+        </div>
+
+
+
+        <div class="row">
+        <div class="col-8 d-flex justify-content-center">
+        <p class="text-center"> 
+
+
+        <?php
+echo $footer_des2;
+    ?>
+        </p>
+         
+        
+      </div>
+        </div>
+
+
+
+
+        
+        <div class="row">
+        <div class="col-8 d-flex justify-content-center">
+        <p class="text-center ">
+
+        <?php
+echo $footer_des3;
+    ?>
+
+
+        </p>
+         
+        
+      </div>
+        </div>
+
+
+
+
+
+
+        
+        <div class="row">
+        <div class="col-8 d-flex justify-content-center">
+        <p class="text-center"
+        
+        
+        <?php
+echo $footer_des4;
+    ?>
+        
+      </p>
+
+
+       
+      
+        
+      </div>
+        </div>
+
+
+
+        <div class="row">
+        <div class="col-8 d-flex justify-content-center">
+
+
+        <a href="#myform">
+
+
+        <button type="button" class="btn btn-primary rounded-pill pulsate"> احجز الحين</button>
+
+        </a>
+     
+      
+        
+      </div>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+       
+        <div class="row">
+        <div class="col-8 d-flex justify-content-center">
+        
+
+        <hr style="height: 3px; background-color: white; width: 450px;">
+        
+      </div>
+        </div>
+        
+       
+ 
+       
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        </div>
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        <div class="col-4"  >
+          <img src="images/footerlogo.png" class="img-fluid" alt="Footer Image"  >
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  
