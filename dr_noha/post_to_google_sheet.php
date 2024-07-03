cf7d1ba4681b408ba27d470b5227c9f96324e844
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-07-03 12:40:25 +0300

    سس

--- dr_noha/post_to_google_sheet.php
+++ dr_noha/post_to_google_sheet.php
@@ -321,4 +321,50 @@ echo "w_api start 2";
 
 
 
+
+  
+
+
+  function send_with_wapi($auth, $profileId, $phone, $message) {
+ 
+
+
+ 
+
+
+        
+
+        
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS => 'auth='.$auth.'&profile_id='.$profileId.'&phone='.$phone.'&msg='.$message.'',
+      CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/x-www-form-urlencoded',
+        'Authorization: 40703bb7812b727ec01c24f2da518c407342559c'
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+    echo $response;
+    
+    
+    
+    
+    
+        }
+    
+        
+        
+
 ?>
