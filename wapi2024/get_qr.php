4d97ba95d1bf4ad32d78875cae02c955e7a28019
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-07-04 20:09:39 +0300

    xx

--- wapi2024/get_qr.php
+++ wapi2024/get_qr.php
@@ -20,7 +20,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-  <meta http-equiv="refresh" content="4">
+  <meta http-equiv="refresh" content="6">
 
   <?php
    
