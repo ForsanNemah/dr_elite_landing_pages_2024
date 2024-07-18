7543ff805c8c738e9e306dfba997c2c82adf7b17
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-07-18 18:23:03 +0300

    s

--- wapi2024/webhok.php
+++ wapi2024/webhok.php
@@ -314,7 +314,7 @@ function get_sheet_msgs($sheet_id,$user_name,$profile_id,$token,$sheet_url,$mess
                 $result = send_with_wapi($token, $profile_id, $chat_id , 'started');
 
 
-                $result = send_with_wapi($token, $profile_id, $chat_id ,$first_object[0]['b'] );
+                $result = send_with_wapi($token, $profile_id, $chat_id ,$data[0]['b'] );
 
             } 
             else{
