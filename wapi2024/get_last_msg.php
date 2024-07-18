4af2c1f271983c17c5658c1aeb33963e8001c1e4
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-07-18 17:59:32 +0300

    ss

--- wapi2024/get_last_msg.php
+++ wapi2024/get_last_msg.php
@@ -3,7 +3,7 @@
 
 
 
-get_last_message_date_if_more_than_one_hour('aedd0dc2-8453','40703bb7812b727ec01c24f2da518c407342559c','966568430828');
+//get_last_message_date_if_more_than_one_hour('aedd0dc2-8453','40703bb7812b727ec01c24f2da518c407342559c','966568430828');
 
 
  
