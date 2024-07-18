f57aefcbcf56d4ab2134c54080f179f76760c117
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-07-18 18:58:32 +0300

    aa

--- wapi2024/webhok.php
+++ wapi2024/webhok.php
@@ -322,7 +322,7 @@ break;
 
 
 
-                $result = send_with_wapi($token, $profile_id, $chat_id , 'started');
+                //$result = send_with_wapi($token, $profile_id, $chat_id , 'started');
                 //$result = send_with_wapi($token, $profile_id, $chat_id , 'started');
                 //$result = send_with_wapi($token, $profile_id, $chat_id , 'started');
 try{
