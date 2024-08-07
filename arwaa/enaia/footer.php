e831a3dfbf958e6cf99d6eaa3fbc5cd85391b2b1
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-08-07 12:52:21 +0300

    z

--- arwaa/enaia/footer.php
+++ arwaa/enaia/footer.php
@@ -6,7 +6,7 @@
  
 
 <div class="py-3 text-center footer-copyright">    
-    <a style="color:blue;" href="#">بواسطة طموح للتسويق الالكتروني </a>
+    <a style="color:blue;" href="https://2moh.net/home/">بواسطة طموح للتسويق الالكتروني </a>
  
  
 
