0a31b8ea04c0e23f8f191ef30aeeb1eda5f88908
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-08-13 16:16:07 +0300

    aa

--- mrads/meswak/hafaralbatin/doctors/lotfi/head.php
+++ mrads/meswak/hafaralbatin/doctors/lotfi/head.php
@@ -111,10 +111,10 @@ u.parentNode.insertBefore(r,u);})(window,document,
 snaptr('init', 'b03a5fe0-a066-4e30-a1e9-28ab9616adf9');
 
 //snaptr('track', 'PAGE_VIEW');
- 
- 
+
 
 snaptr('init', 'b67a49ad-d1f1-445f-941f-31aea29bc8e4');
+snaptr('init', 'b2436d09-c13e-4280-9181-caa1960c5448');
 
 
 snaptr('track', 'PAGE_VIEW');
