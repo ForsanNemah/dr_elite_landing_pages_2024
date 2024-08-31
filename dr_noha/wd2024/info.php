4b43347d1566cf34c657554bc9e7b80b77fedb35
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-08-31 17:29:40 +0300

    aa

---
+++ dr_noha/wd2024/info.php
@@ -0,0 +1,157 @@
+<?php
+
+
+$website_name="
+
+مجمع عيادات ايفا في حفر الباطن
+
+ 
+";
+
+$dr_name="
+
+<br>
+عروض اليوم الوطني  2024
+";
+
+$phone_main="966505506913";
+
+$action_url="https://script.google.com/macros/s/AKfycbxp69HiC0Y-PirGPkMoJ5c1UFesdbh7EITIWvndxee4Pk66jgLW_BtRJzdMUy63gHc/exec";
+
+$sheet_url="https://docs.google.com/spreadsheets/d/1Cw50UoyyK4IobN-9q5YLT7LorvrF1xpPkV0_PTJK-MI/edit?usp=sharing";
+
+$form1_des="
+
+عروض اليوم الوطني
+";
+$cta=0;
+$cta_text="احجز الان";
+$form1_des2="
+
+سجل بياناتك وسيتم التواصل معكم مباشرة 
+";
+
+$form1_des3="
+
+
+ 
+
+";
+
+
+
+
+
+$form2_des="
+
+قم بتعبئة البيانات
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+قمة الإنتشار للتسويق الإلكتروني  
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+سجل تجاري رقم 
+
+ <br>
+2051251848
+<br>
+<a href= 'mailto:info@intshar.net' style='color: white;'>info@intshar.net</a>
+
+<br>
+
+<a href='tel:'.$phone_main.' style='color: white;'>$phone_main</a>
+
+<br>
+المملكة العربية السعودية-الخبر
+";
+
+
+
+$footer_des4="
+
+المملكة العربية السعودية-الخبر
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$map=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$footer2=0;
+$w_app_button=0;
+$api_notification=0;
+$ad_source="snap ";
+$header1=1;
+$parteners=0;
+$about_image=0;
+
+$services_list=1;
+$taby_and_tamara=1;
+$footer_cr=1;
+$videos=0;
+
+
+
+
+
+$pixel_id = "77ec2e1e-c2bb-468f-8d2e-f99b7ec9983c";
+$authorization_token = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI0NjIwODIzLCJzdWIiOiJhMTAzMjVhMi0zMzU1LTRkMjMtODhmOC04MmFmZTc3NzYzOTR-UFJPRFVDVElPTn4yNTZmYTNlNi1iZDhmLTQwMTctYTQ2MC0yYjU2OGI4MDJiMWQifQ.7-CnLhZlNw3mr9vkdWMMtVoUz0CONBGZibDPEOEo8Es";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+*/
+
+?>
