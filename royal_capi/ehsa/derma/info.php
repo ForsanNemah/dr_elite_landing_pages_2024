731d011256824c5209426033440cc203e88a7253
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-09-01 13:13:26 +0300

    s

---
+++ royal_capi/ehsa/derma/info.php
@@ -0,0 +1,158 @@
+<?php
+
+
+$website_name="
+
+
+مجمع رويال الطبي 
+ 
+";
+
+$dr_name="
+
+
+
+
+
+";
+
+$phone_main="966590013452";
+
+$action_url="https://script.google.com/macros/s/AKfycbybL7vKRdTIZA0KJmx83JzOSdSgNzJhgYzrZ-YSgzigw5V3UXO28h6FfU3phIXVlynZOg/exec";
+
+$sheet_url="https://docs.google.com/spreadsheets/d/1Cw50UoyyK4IobN-9q5YLT7LorvrF1xpPkV0_PTJK-MI/edit?usp=sharing";
+
+$form1_des="
+
+عروض اليوم الوطني
+";
+$cta=0;
+$cta_text="احجز الان";
+$form1_des2="
+
+سجل بياناتك وسيتم التواصل معكم مباشرة 
+";
+
+$form1_des3="
+
+
+ 
+
+";
+
+
+
+
+
+$form2_des="
+
+قم بتعبئة البيانات
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+قمة الإنتشار للتسويق الإلكتروني  
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+سجل تجاري رقم 
+
+ <br>
+2051251848
+<br>
+<a href= 'mailto:info@intshar.net' style='color: white;'>info@intshar.net</a>
+
+<br>
+
+<a href='tel:'.$phone_main.' style='color: white;'>$phone_main</a>
+
+<br>
+المملكة العربية السعودية-الخبر
+";
+
+
+
+$footer_des4="
+
+المملكة العربية السعودية-الخبر
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$map=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$footer2=0;
+$w_app_button=0;
+$api_notification=0;
+$ad_source="snap ";
+$header1=1;
+$parteners=0;
+$about_image=0;
+
+$services_list=1;
+$taby_and_tamara=1;
+
+
+
+
+
+
+$pixel_id = "b2436d09-c13e-4280-9181-caa1960c5448";
+$authorization_token = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI0NjcyOTkxLCJzdWIiOiI1ZThlMTgyNC1lZDg0LTQ1MDktYjcxNC04MzNkODVkMmJkYjJ-UFJPRFVDVElPTn41NmFhMmEwZS1lZWRkLTRmYjktOTVmZi04Mjk1MmMwNjkzOGUifQ.jFUSPqfde51nBjceQ2kV90ESceF9F2N-s30gr08y4HM";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+*/
+
+?>
