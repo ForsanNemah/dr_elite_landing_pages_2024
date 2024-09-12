cc0963042ced1e74aa86fae67ab8b89895232827
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-09-12 17:52:01 +0300

    as

---
+++ royal_shirton-bh/dr_eyman/doctors/mari/info.php
@@ -0,0 +1,131 @@
+<?php
+
+
+$website_name="
+
+
+مجمع رويال الطبي
+ 
+";
+
+$dr_name="
+
+ 
+
+
+";
+
+$phone_main="966541000937";
+
+$action_url="https://script.google.com/macros/s/AKfycbzNK05kyj3OjIq_x3gnSuJBNLq2z6NRu7IAzsmK75ElFBKaf00PCDx8tfUmkkQM85y_/exec";
+
+$sheet_url="https://docs.google.com/spreadsheets/d/1Cw50UoyyK4IobN-9q5YLT7LorvrF1xpPkV0_PTJK-MI/edit?usp=sharing";
+
+$form1_des="
+
+عروض الصيف
+";
+$cta=0;
+$cta_text="احجز الان";
+$form1_des2="
+
+سجل بياناتك وسيتم التواصل معكم مباشرة 
+";
+
+$form1_des3="
+
+
+ 
+
+";
+
+
+
+
+
+$form2_des="
+
+قم بتعبئة البيانات
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+قمة الإنتشار للتسويق الإلكتروني  
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+سجل تجاري رقم 
+
+ <br>
+2051251848
+<br>
+<a href= 'mailto:info@intshar.net' style='color: white;'>info@intshar.net</a>
+
+<br>
+
+<a href='tel:'.$phone_main.' style='color: white;'>$phone_main</a>
+
+<br>
+المملكة العربية السعودية-الخبر
+";
+
+
+
+$footer_des4="
+
+المملكة العربية السعودية-الخبر
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$map=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$footer2=0;
+$w_app_button=1;
+$api_notification=1;
+$ad_source="snap ";
+$header1=1;
+$parteners=0;
+$about_image=0;
+
+$services_list=1;
+$taby_and_tamara=1;
+
+
+?>
