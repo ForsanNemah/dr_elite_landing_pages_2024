d90adbf79d70be0b5ceebe7247e0de89e9efc285
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-09-25 12:55:16 +0300

    ss

--- kayan/skin/w_app_form.php
+++ kayan/skin/w_app_form.php
@@ -1,9 +1,9 @@
 <div class="form-container" id="w_app_form" data-aos-duration="1500"  data-aos="zoom-in-up">
   <div class="row justify-content-center custom_row_margin">
     <div class="col-lg-6">
-      <div class="card mt-5">
+      <div class="mt-5 card">
         <div class="card-body form_bg" >
-          <h2 class="card-title text-center mb-4" style="color: white;">
+          <h2 class="mb-4 text-center card-title" style="color: white;">
           
           
           <?php
@@ -186,7 +186,7 @@ echo 'File not found.';
             <img id="loading2" src="images/loading.gif" alt="Italian Trulli"  width="100" height="100" >
 
 
-              <button id="form2_button" type="submit" class="btn btn-primary   rounded-pill"> فتح واتساب</button>
+              <button id="form2_button" type="submit" class="btn btn-primary rounded-pill"> فتح واتساب</button>
             </div>
           </form>
         </div>
