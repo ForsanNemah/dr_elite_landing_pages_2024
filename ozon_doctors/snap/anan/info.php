5a528b5f2499967926b0570ab927fa3e5556a2b0
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-10-14 17:49:53 +0300

    s

---
+++ ozon_doctors/snap/anan/info.php
@@ -0,0 +1,136 @@
+<?php
+
+
+$website_name="
+
+
+مجمع اوزون الطبي 
+ 
+";
+
+$dr_name="
+
+
+";
+
+$phone_main="966565383306";
+
+$action_url="https://script.google.com/macros/s/AKfycbwIJiggqfLvvnV6LZqeYRLE99Dhqxqa0-5p_EOH4g_UO4q4rDBFdatQ5MwWCYoYUFcJ/exec";
+
+$sheet_url="https://docs.google.com/spreadsheets/d/1Cw50UoyyK4IobN-9q5YLT7LorvrF1xpPkV0_PTJK-MI/edit?usp=sharing";
+
+$form1_des="
+احجز الان
+";
+$cta=0;
+$cta_text="احجز الان ";
+$form1_des2="
+
+ 
+خصومات تصل الى 50%
+";
+
+$form1_des3="
+
+
+ 
+
+";
+
+
+
+
+
+$form2_des="
+
+قم بتعبئة البيانات
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+قمة الإنتشار للتسويق الإلكتروني  
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+سجل تجاري رقم 
+
+ <br>
+2051251848
+<br>
+<a href= 'mailto:info@intshar.net' style='color: white;'>info@intshar.net</a>
+
+<br>
+
+<a href='tel:'.$phone_main.' style='color: white;'>$phone_main</a>
+
+<br>
+المملكة العربية السعودية-الخبر
+";
+
+
+
+$footer_des4="
+
+المملكة العربية السعودية-الخبر
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$map=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$footer2=0;
+$w_app_button=0;
+$api_notification=1;
+$ad_source="snap ";
+$header1=1;
+$parteners=0;
+$about_image=0;
+
+$services_list=1;
+$taby_and_tamara=1;
+
+
+
+$wapi_profile_id="a0aca078-18a5";
+$wapi_token="9f9151b56d756354026b368ecc644edec19343b4";
+$group_id="120363322994963509";
+
+
+
+?>
