0bf6c467947ad3f688b9fe0a9ca32059ca00aaaa
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-10-20 02:09:57 +0300

    ss

--- wapi2024/ab.php
+++ wapi2024/ab.php
@@ -46,10 +46,10 @@ $to_user = $event['messages'][0]['to'];
 
 
 
-//$to_user==$group_a
-if(1==1){
+//
+if($to_user==$group_a){
 
-   // $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453',$group_b, $message);
+    $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453',$group_b, $message);
 
 }
 
