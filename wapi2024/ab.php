380dfc7b87327aa157e8c0fe55efc7242c734d68
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-10-21 02:50:50 +0300

    ss

--- wapi2024/ab.php
+++ wapi2024/ab.php
@@ -57,20 +57,17 @@ $type = $event['messages'][0]['type'];
 
 
 
-
+/*
 $group_b = lookupValue($chat_id, $group1, $group2);
-//echo $result; // Output: yellow
-
-
 $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', $profile_id,'966568430828@c.us', $group_b);
 
 /*
  
 $group_a="120363331373447780@g.us";
-$group_b="120363348465495758@g.us";
+
 */
 
-
+$group_b="120363348465495758@g.us";
 
 
 
