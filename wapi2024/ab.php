64ac35775589df508db159d581cf8a3503b61db6
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-10-21 02:42:39 +0300

    ass

--- wapi2024/ab.php
+++ wapi2024/ab.php
@@ -82,9 +82,9 @@ if($chat_id==$group_a && $chat_id==$to_user){
 
 
 
-    wapiforword('aedd0dc2-8453', '40703bb7812b727ec01c24f2da518c407342559c', $message_id, $group_b);
+    wapiforword($profile_id, '40703bb7812b727ec01c24f2da518c407342559c', $message_id, $group_b);
 
-    $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453',$group_b, $caption);
+    $result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', $profile_id,$group_b, $caption);
 
 
 }
