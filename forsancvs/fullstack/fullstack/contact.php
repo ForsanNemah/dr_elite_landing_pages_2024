58845231ca7f73013ce5cef9d0c7e0932f038feb
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-10-26 17:01:51 +0300

    s

---
+++ forsancvs/fullstack/fullstack/contact.php
@@ -0,0 +1,85 @@
+
+<section id="contact" class="contact section">
+
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+  <h2>تواصل معي </h2>
+  <p>يمكنك التواصل معي الان بكل سهولة سواء للعمل او الحصول على استشارة </p>
+</div><!-- End Section Title -->
+
+<div class="container" data-aos="fade" data-aos-delay="100">
+
+  <div class="row gy-4">
+
+    <div class="col-lg-4">
+      <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="200">
+        <i class="flex-shrink-0 bi bi-geo-alt"></i>
+        <div>
+          <h3>العنوان</h3>
+          <p>السعودية-الخبر</p>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="300">
+        <i class="flex-shrink-0 bi bi-telephone"></i>
+        <div>
+          <h3>اتصل بي </h3>
+          <p>966568430828</p>
+        </div>
+      </div><!-- End Info Item -->
+
+      <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
+        <i class="flex-shrink-0 bi bi-envelope"></i>
+        <div>
+          <h3>كلمني ايميل</h3>
+          <p>info@4sansoft.com</p>
+        </div>
+      </div><!-- End Info Item -->
+
+    </div>
+
+    <div class="col-lg-8">
+      <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
+        <div class="row gy-4">
+
+          <div class="col-md-12">
+             
+
+
+
+            <select name="services" id="services" class="form-control">
+            <option value="0"> اختر الخدمة</option>
+  <option value="web_dev">برمجة المواقع والمنصات الالكترونية</option>
+  <option value="team_management">خدمة ادارة الفرق البرمجية</option>
+  <option value="consultation">الحصول على استشارة</option>
+</select>
+
+          </div>
+
+          <div class="col-md-12 ">
+            <input type="text" class="form-control" name="name" placeholder="الاسم  " required="">
+          </div>
+
+          <div class="col-md-12"  >
+            <input type="number" class="form-control" name="phone" placeholder="رقم الجوال" required="">
+          </div>
+
+          <div class="col-md-12" hidden>
+            <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
+          </div>
+
+          <div class="text-center col-md-12">
+            
+
+            <button type="submit">إرسال</button>
+          </div>
+
+        </div>
+      </form>
+    </div><!-- End Contact Form -->
+
+  </div>
+
+</div>
+
+</section>
