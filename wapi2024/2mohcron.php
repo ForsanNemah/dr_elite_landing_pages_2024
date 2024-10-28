54707959dbee161559f04d9f9474322853589cc7
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-10-28 16:46:49 +0300

    ss

---
+++ wapi2024/2mohcron.php
@@ -0,0 +1,82 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+$message='
+ شيت حملات م سامي 
+
+https://docs.google.com/spreadsheets/d/1DJ2fD_Ga4jhy8-bsopdlH2f6zfbmh1rUPxXn9p2BfFQ/edit?usp=sharing
+
+شيت حملات م اسلام
+
+https://docs.google.com/spreadsheets/d/14Fzvj2cNzhw5dXUQmrqUD1UCZRwbQm3H1AOr6YsFsDo/edit?usp=sharing
+';
+
+
+
+$message = str_replace(["\r\n", "\r", "\n"], "\\n", $message);
+
+
+$result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453','120363294487606942@g.us', $message );
+$result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453','966599708654@c.us', $message );
+
+
+
+function send_with_wapi($auth, $profileId, $phone, $message) {
+ 
+
+
+ 
+
+
+        
+
+        
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://ads.2moh.net/wapi2024/',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_POSTFIELDS => 'auth='.$auth.'&profile_id='.$profileId.'&phone='.$phone.'&msg='.$message.'',
+      CURLOPT_HTTPHEADER => array(
+        'Content-Type: application/x-www-form-urlencoded',
+        'Authorization: 40703bb7812b727ec01c24f2da518c407342559c'
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+    echo $response;
+    
+    
+    
+    
+    
+        }
+        
+        
+         
+     
+      
+        
+
+
+
+
+
+
+?>
