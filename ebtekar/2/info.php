efce7e5dc8946ddfa60c3f3504f956d354ae5134
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-11-16 09:22:18 +0300

    ss

---
+++ ebtekar/2/info.php
@@ -0,0 +1,169 @@
+<?php
+
+
+$website_name="
+
+المعهد الصحي اليمني      
+
+
+ 
+";
+
+$department_name="      معهد نخبة الابتكار         ";
+
+$dr_name="
+
+<br>
+";
+
+$phone_main="967715293655";
+$group_id="120363365701504514";
+$logo="footer_logo/ebtekar.png";
+
+$dental_services_select=1;
+$derma_services_select=0;
+
+
+$action_url="https://script.google.com/macros/s/AKfycbxU1ysaYoT_m-KVi-ya7dPVJJ6cZVDRyfa7XcjERfp8hs2CtK77gOggIYgGv5ZGfAzM/exec";
+
+$sheet_url="https://docs.google.com/spreadsheets/d/1Cw50UoyyK4IobN-9q5YLT7LorvrF1xpPkV0_PTJK-MI/edit?usp=sharing";
+
+$form1_des="
+
+سجل معنا    
+"; 
+$cta=0;
+$cta_text="سجل الان";
+$form1_des2="
+
+واحصل على خصم يصل الى 50%";
+
+$form1_des3="
+
+
+ 
+
+";
+
+
+
+
+
+$form2_des="
+
+قم بتعبئة البيانات
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+قمة الإنتشار للتسويق الإلكتروني  
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+سجل تجاري رقم 
+
+ <br>
+2051251848
+<br>
+<a href= 'mailto:info@intshar.net' style='color: white;'>info@intshar.net</a>
+
+<br>
+
+<a href='tel:'.$phone_main.' style='color: white;'>$phone_main</a>
+
+<br>
+المملكة العربية السعودية-الخبر
+";
+
+
+
+$footer_des4="
+
+المملكة العربية السعودية-الخبر
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$map=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$footer2=0;
+$w_app_button=0;
+$api_notification=1;
+$group_notification=1;
+$ad_source="snap ";
+$header=0;
+$parteners=0;
+$about_image=0;
+
+$services_list=1;
+$taby_and_tamara=1;
+$footer_cr=1;
+$videos=0;
+$snap_capi=0;
+$w_btn_only=1;
+$pm_register=0;
+
+
+
+
+
+$pixel_id = "77ec2e1e-c2bb-468f-8d2e-f99b7ec9983c";
+$authorization_token = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI0NjIwODIzLCJzdWIiOiJhMTAzMjVhMi0zMzU1LTRkMjMtODhmOC04MmFmZTc3NzYzOTR-UFJPRFVDVElPTn4yNTZmYTNlNi1iZDhmLTQwMTctYTQ2MC0yYjU2OGI4MDJiMWQifQ.7-CnLhZlNw3mr9vkdWMMtVoUz0CONBGZibDPEOEo8Es";
+
+
+
+$wapi_profile_id="aedd0dc2-8453";
+$wapi_token="40703bb7812b727ec01c24f2da518c407342559c";
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+*/
+
+?>
