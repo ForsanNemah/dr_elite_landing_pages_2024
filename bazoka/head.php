127c25c8d99f363aeec9ee23f5425abed42c6def
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-11-23 00:15:21 +0300

    s

--- bazoka/head.php
+++ bazoka/head.php
@@ -351,4 +351,42 @@ var sha256 = function sha256(ascii) {
   }
 </script>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "p2q78o1f7y");
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+
 </head>
