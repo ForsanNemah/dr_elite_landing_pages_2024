cf93a707635988e9cf8d5e9f098cdde23eec029c
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-11-30 16:37:36 +0300

    ss

---
+++ gdfe/index.php
@@ -0,0 +1,23 @@
+<iframe src="https://drive.google.com/embeddedfolderview?id=1pSfWJxkLnylLRjZwfGJhVsG0xVOof7_1#grid" style="width:100%; height:600px; border:0;"></iframe>
+
+
+
+
+
+
+
+
+
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Embed Google Drive Folder</title>
+</head>
+<body>
+    <h1>My Google Drive Folder</h1>
+    <!-- Embed the folder -->
+    <iframe src="https://drive.google.com/embeddedfolderview?id=1pSfWJxkLnylLRjZwfGJhVsG0xVOof7_1#list" width="100%" height="500" frameborder="0"></iframe>
+</body>
+</html>
