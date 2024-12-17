a8639ea06c109f182a9829dbdb2dc6f18f0b9ca6
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2024-12-17 12:24:55 +0300

    sss

---
+++ mersah/google_source/info.php
@@ -0,0 +1,170 @@
+<?php
+
+
+$website_name="
+
+مكتب  تلال الظهران 
+
+ 
+";
+
+$department_name="       مكتب  تلال الظهران               ";
+
+$dr_name="
+
+ 
+";
+
+$phone_main="966502229159";
+$group_id="120363323006566221";
+$logo="footer_logo/telal.jpg";
+
+$dental_services_select=0;
+$derma_services_select=0;
+
+
+$action_url="https://script.google.com/macros/s/AKfycbx1GWDPCxNopXBewgnlBGyj9oXCMt31-Lb5j32-iU8uHnY_A8YZEI4MwK9C1VsA14r2/exec";
+
+$sheet_url="https://docs.google.com/spreadsheets/d/1Cw50UoyyK4IobN-9q5YLT7LorvrF1xpPkV0_PTJK-MI/edit?usp=sharing";
+
+$form1_des="
+
+احصل على  المدبرة المنزلية       
+"; 
+$cta=0;
+$cta_text="احجز الان";
+$form1_des2="
+
+عروض نهاية  العام (لاول 10 طلبات)
+😍       
+";
+
+$form1_des3="
+
+
+ 
+
+";
+
+
+
+
+
+$form2_des="
+
+قم بتعبئة البيانات
+
+";
+
+
+$form2_des2="
+
+وسيتم تحويلك الى واتساب مباشرة
+
+";
+
+
+
+
+$footer_des1="
+
+قمة الإنتشار للتسويق الإلكتروني  
+
+";
+
+
+
+$footer_des2="
+
+ 
+
+";
+
+
+
+$footer_des3="
+
+سجل تجاري رقم 
+
+ <br>
+2051251848
+<br>
+<a href= 'mailto:info@intshar.net' style='color: white;'>info@intshar.net</a>
+
+<br>
+
+<a href='tel:'.$phone_main.' style='color: white;'>$phone_main</a>
+
+<br>
+المملكة العربية السعودية-الخبر
+";
+
+
+
+$footer_des4="
+
+المملكة العربية السعودية-الخبر
+
+";
+
+
+
+
+
+
+
+
+$before_after=0;
+$map=0;
+$services=0;
+$derma_services=0;
+$services2=0;
+$faq=0;
+$process=0;
+$services_images=0;
+$footer=1;
+$footer2=0;
+$w_app_button=0;
+$api_notification=1;
+$group_notification=1;
+$ad_source="google ";
+$header=0;
+$parteners=0;
+$about_image=0;
+
+$services_list=1;
+$taby_and_tamara=0;
+$footer_cr=1;
+$videos=0;
+$snap_capi=0;
+$w_btn_only=1;
+$pm_register=0;
+
+
+
+
+
+$pixel_id = "77ec2e1e-c2bb-468f-8d2e-f99b7ec9983c";
+$authorization_token = "eyJhbGciOiJIUzI1NiIsImtpZCI6IkNhbnZhc1MyU0hNQUNQcm9kIiwidHlwIjoiSldUIn0.eyJhdWQiOiJjYW52YXMtY2FudmFzYXBpIiwiaXNzIjoiY2FudmFzLXMyc3Rva2VuIiwibmJmIjoxNzI0NjIwODIzLCJzdWIiOiJhMTAzMjVhMi0zMzU1LTRkMjMtODhmOC04MmFmZTc3NzYzOTR-UFJPRFVDVElPTn4yNTZmYTNlNi1iZDhmLTQwMTctYTQ2MC0yYjU2OGI4MDJiMWQifQ.7-CnLhZlNw3mr9vkdWMMtVoUz0CONBGZibDPEOEo8Es";
+
+
+
+$wapi_profile_id="a0aca078-18a5";
+$wapi_token="9f9151b56d756354026b368ecc644edec19343b4";
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+*/
+
+?>
