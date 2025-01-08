b07aed3cedd140860db881eb664df2c7f397f9ed
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-01-09 00:44:12 +0300

    ss

--- wapi2024/airesponse.php
+++ wapi2024/airesponse.php
@@ -11,6 +11,46 @@ $chat_gpt_phone="18002428478";
 
 
 
+$conditions='
+
+
+انت مندوب في وكالة تسويق الكتروني  مهامك بيع الخدمات التالية
+
+ادارة حسابات السوشال ميديا  
+عمل الحملات الممولة  
+برمجة المواقع الالكترونية  
+برمجة تطبيقات الموبايل  
+انشاء المتاجر الالكترونية  
+خدمة واتساب سندر  
+خدمة واتساب شات بوت  
+تحسين محركات البحث seo  
+
+وفي حال تم السؤال عن احد الخدمات قم بالتفصيل في الاجابة  
+
+التقنيات المستخدمة في البرمجة  
+wordpress  
+laravel  
+php  
+html  
+css  
+boostrap  
+java script  
+
+في حال سأل العميل عن الاسعار  
+سيقوم احد المسؤولين بالتواصل معك  
+
+التعليمات  
+لا ترد في اي موضوع خارج هذه المواضيع  
+الرد يكون باللهجة السعودية  
+تجنب استخدام النجمات في الردود  
+
+
+
+';
+
+
+
+
 
 $data = file_get_contents("php://input");
 
@@ -53,6 +93,67 @@ $contact_name = $event['messages'][0]['contact_name'];
 
 
 $result = send_with_wapi($token, $profile_id,'966568430828@c.us', $message." my msg to " );
+
+
+
+
+
+
+
+
+
+$result = send_with_wapi($token, $profile_id,$chat_gpt_phone.'@c.us', $message );
+
+
+
+
+
+$last_message=get_last_message_date_if_more_than_one_hour($token,$profile_id,$chat_gpt_phone);
+
+
+
+
+//print_r($last_message);
+
+while ($last_message[1] == 1) {
+
+    global $last_message;
+
+    $last_message=get_last_message_date_if_more_than_one_hour($token,$profile_id,$chat_gpt_phone);
+
+    //print_r($last_message);
+
+   
+}
+
+//echo $last_message[0];
+
+
+$result = send_with_wapi($token, $profile_id,$chat_id,$last_message[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -133,6 +234,95 @@ function send_with_wapi($auth, $profileId, $phone, $message) {
          
      
       
+
+
+        
+function get_last_message_date_if_more_than_one_hour($token,$profile_id,$chat_id) {
+
+
+
+
+
+
+
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://wappi.pro/api/sync/messages/get?profile_id='.$profile_id.'&chat_id='.$chat_id.'&limit=1&mark_all=true&order=desc',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+      CURLOPT_HTTPHEADER => array(
+        'Authorization: '.$token
+      ),
+    ));
+    
+    $response = curl_exec($curl);
+    
+    curl_close($curl);
+
+
+    //echo $response."<br>";
+    $data = json_decode($response, true);
+   
+    //print_r($data);
+
+$timestamp = $data['messages'][0]['time'];
+$body = $data['messages'][0]['body'];
+$fromMe = $data['messages'][0]['fromMe'];
+
+
+
+$last_message= [$body, $fromMe];
+
+
+return $last_message;
+
+//echo $body;
+//echo $fromMe."for me";
+
+
+//echo $body;
+
+
+
+
+
+
+
+
+/*
+$normalTime = date("Y-m-d H:i:s", $timestamp);
+ $normalTime;
+    echo $normalTime;
+
+
+    //echo isCurrentTimeGreaterThanNormalTimeByOneHour($normalTime);
+ 
+
+    if(isCurrentTimeGreaterThanNormalTimeByOneHour($normalTime)){
+
+      return true;
+
+
+    }
+    else{
+
+      return false;
+
+    }
+    */
+    
+    
+    
+    }
+    
+     
+
 
 
 
