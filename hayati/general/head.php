16fb306d463b90b7d1dc35fe06abd3aaa7a2dd55
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-01-13 11:38:17 +0300

    ss

--- hayati/general/head.php
+++ hayati/general/head.php
@@ -171,6 +171,7 @@ snaptr('init', 'b2436d09-c13e-4280-9181-caa1960c5448');
 //snaptr('track', 'PAGE_VIEW');
  
  
+ 
 
 snaptr('init', 'b67a49ad-d1f1-445f-941f-31aea29bc8e4');
 snaptr('init', 'b00f5a25-e6f1-462e-954c-e0ad9a423e01');
