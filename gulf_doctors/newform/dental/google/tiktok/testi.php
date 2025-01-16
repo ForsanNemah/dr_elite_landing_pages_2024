6e231add7b28c0e005937a4f73ab931363f91e0e
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-01-16 23:16:38 +0300

    ff

---
+++ gulf_doctors/newform/dental/google/tiktok/testi.php
@@ -0,0 +1,98 @@
+ 
+
+ <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+ 
+
+
+
+
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+
+ 
+
+
+ 
+  
+
+
+
+
+
+
+
+
+<section id="customers_testi">
+      <div class="st-height-b120 st-height-lg-b80"></div>
+      <div class="container">
+        <div class="st-section-heading st-style1">
+          <h2 class="st-section-heading-title">ماذا قالو عنا </h2>
+          <div class="st-seperator">
+            <div class="st-seperator-left wow fadeInLeft" data-wow-duration="1s" data-wow-delay="0.2s"></div>
+            <div class="st-seperator-center"><img src="assets/img/icons/4.png" alt="icon"></div>
+            <div class="st-seperator-right wow fadeInRight" data-wow-duration="1s" data-wow-delay="0.2s"></div>
+          </div>
+          <div class="st-section-heading-subtitle">
+            
+      عينة من اراء  عملائنا مما يجعلنا بلا شك في  الصدارة 
+      من ناحية الجودة المقدمة 
+          
+          
+   
+
+          </div>
+        </div>
+
+
+
+ 
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+        <?php
+
+make_slider("testi","one","   معنا انت اجمل من ما تتصورين     ","");
+
+?>
+
+
+
+
+
+
+        <div class="st-height-b40 st-height-lg-b40">
+
+
+        
+        </div>
+      </div>
+
+
+
+
+     
+      <div class="st-height-b120 st-height-lg-b80"></div>
+    </section>
+
+
+ 
