b414138fa98d72d458f347f06bd39b65eea77272
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-01-21 02:57:34 +0300

    d

--- enaia/info.php
+++ enaia/info.php
@@ -22,7 +22,7 @@ $gmap_url="https://goo.gl/maps/S1Zko9MvKiYzqPQV9";
 
 $site_name="مجمع العناية الطبي لطب الاسنان ";
 $dr_name="  ";
-$add_title="عروض الصيف 2024 ";
+$add_title=" عروض العناية     ";
 
 $sidebar1_msg="نحلم ونحقق";
 $sidebar2_msg="خدماتنا ";
