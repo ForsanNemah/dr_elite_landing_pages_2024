fc2e2bc4ecf65878fc05460ff712ba788ace6aa6
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-02-09 18:21:11 +0300

    ss

--- 2moh_task/2moh_tasks2/post_to_google_sheet.php
+++ 2moh_task/2moh_tasks2/post_to_google_sheet.php
@@ -99,7 +99,7 @@ if(1==1){
 
     $w_app_msg5 = str_replace("\n", "\\n",  $w_app_msg5);
 
-$result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453', '120363216158625125@g.us',  $w_app_msg5);
+$result = send_with_wapi('40703bb7812b727ec01c24f2da518c407342559c', 'aedd0dc2-8453', '120363293040099791@g.us',  $w_app_msg5);
 
 echo print_r($result) ;
 
