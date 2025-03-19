9f93bb54dcb0be8080c726f5c2676f5bb10e03bc
ForsanNemah <51035839+ForsanNemah@users.noreply.github.com>
2025-03-19 16:48:23 +0300

    ss

--- aljameel2025/dental/head.php
+++ aljameel2025/dental/head.php
@@ -173,7 +173,7 @@ snaptr('init', 'b2436d09-c13e-4280-9181-caa1960c5448');
  
 snaptr('init', 'f28443d6-e935-4551-9d40-2f54564592b5');
 
-
+snaptr('init', 'c530b9a9-f8d4-48df-ba97-3958aae4f9a6');
 
 snaptr('init', 'b67a49ad-d1f1-445f-941f-31aea29bc8e4');
 snaptr('init', '390ecce4-ff7f-4ad0-a7fb-e5e65acff702');
